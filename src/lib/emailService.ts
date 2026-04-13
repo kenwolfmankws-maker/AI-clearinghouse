10969444819d8f83e1da9d5ba50bd833b44e9ebd
kenwolfman <kenwolfman.kws@gmail.com>
2026-04-13 13:31:44 -0600

    Add emailService with welcome and invite email methods

---
+++ src/lib/emailService.ts
@@ -0,0 +1,10 @@
+export const emailService = {
+  sendWelcomeEmail: async () => {
+    console.log("email disabled");
+    return { data: null, error: null };
+  },
+  sendInviteEmail: async () => {
+    console.log("email disabled");
+    return { data: null, error: null };
+  },
+};
